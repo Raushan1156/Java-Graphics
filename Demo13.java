093b59a5e20eced79ae1e1683da9efa8c2563746
raushan1156 <iamraushankumar56@gmail.com>
2021-08-02 23:50:30 +0530

    Line size decreasing from both top and bottom.

--- Demo13.java
+++ Demo13.java
@@ -32,11 +32,11 @@ public class Demo13 extends Frame {
             Thread.sleep(500);
              }
         catch(Exception e){}
-     // Line is not at perfect position.
         g.drawLine(x1,y1,x2,y2);   
         x1=x1+80;
         x2=x2+80;   
-        y1=y1+20;  
+        y1=y1+20;
+        y2=y2-20;  
     }
 
     }
