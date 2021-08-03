126e9e7562a6195d6457ecace630e256d97551dd
raushan1156 <iamraushankumar56@gmail.com>
2021-08-03 19:25:02 +0530

    Shining moon,colorful Ashoka Chakra and colorful arcs at 15degrees.

--- Demo18.java
+++ Demo18.java
@@ -1,4 +1,4 @@
-// Colorful Ashoka Chakra having random colors everytime
+// Shining moon with different colors
 import java.awt.Frame;
 import java.awt.Color;
 import java.awt.event.*;
@@ -25,7 +25,12 @@ public class Demo18 extends Frame {
         int x2=100;
         int y2=100;
         int sa=0;
-        int ma=3; // margin pixel
+        
+        
+        // For drawing colorful arcs having 15 degrees, Change margin to the sa i.e. 15. 
+      //  int ma=3; // margin pixel
+      int ma=15;
+
         g.setColor(Color.white);
         
         for(int i=0;true;i++){
@@ -35,8 +40,24 @@ public class Demo18 extends Frame {
 
             Color c= new Color(r,g1,b);
             g.setColor(c);
+
+
+
+           /*  Colorful  Ashoka Chakra
             g.fillArc(x1,y1,x2,y2,sa,ma);
-          sa=sa+15;
+          sa=sa+15;*/
+
+
+
+          g.fillArc(x1,y1,x2,y2,sa,ma);
+          sa+=15;
+
+
+
+         // g.fillArc(x1,y1,x2,y2,sa,360);        // Shining Moon
+
+
+
             try{
                Thread.sleep(20);
             }
